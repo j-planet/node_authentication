7826e6cf8cb483b911c89a00a6c2fde64943d307
jennyyuejin <jin.yuejenny@gmail.com>
2016-03-28 01:26:51 -0700

    defined user schema and class

--- models/user.js
+++ models/user.js
@@ -3,15 +3,18 @@
  */
 
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-// Define our model
-const userSchema = new Schema({
-    email: String,
+
+// Define our model. Mongoose schema.
+const userSchema = new mongoose.Schema({
+    email: { type: String, unique: true, lowercase: true},
     password: String
 });
 
-// create the model class
+
+// create the model class. represents all users.
+const UserModelClass = mongoose.model('user', userSchema);
 
 
 // export the model
+module.exports = UserModelClass;
